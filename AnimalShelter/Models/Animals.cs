72f7b8b2237664146036d16a48ddebcb468e18fe
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-18 12:28:15 -0700

    Finished AnimalShelterContext and removed the DateTime variable.

--- AnimalShelter/Models/Animals.cs
+++ AnimalShelter/Models/Animals.cs
@@ -18,8 +18,7 @@ namespace AnimalShelterApi.Models
     [StringLength(255, ErrorMessage = "You must put if the animal is female or male.")]
     public string MaleFemale { get; set; }
     [Required]
-    [Range(0, 255, ErrorMessage = "Age must be between 0 and 80.")]
+    [Range(0, 255, ErrorMessage = "Age must be between 0 and 507.")]
     public string AnimalAge { get; set; }
-    public DateTime Date { get; set; }
   }
 }
