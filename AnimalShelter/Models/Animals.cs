8469cf0f1639ea8cc0f8cfa600c34d8f4a046078
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-18 11:48:40 -0700

    Added more animal identifiers in Animals model.

--- AnimalShelter/Models/Animals.cs
+++ AnimalShelter/Models/Animals.cs
@@ -14,6 +14,12 @@ namespace AnimalShelterApi.Models
     [Required]
     [StringLength(250, ErrorMessage = "This animal must have a type.")]
     public string AnimalType { get; set; }
+    [Required]
+    [StringLength(255, ErrorMessage = "You must put if the animal is female or male.")]
+    public string MaleFemale { get; set; }
+    [Required]
+    [Range(0, 255, ErrorMessage = "Age must be between 0 and 80.")]
+    public string AnimalAge { get; set; }
     public DateTime Date { get; set; }
   }
 }
