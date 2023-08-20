04bf16a930f12f94d5b947d8517a4cc97e5f5ac0
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-20 13:18:24 -0700

    Finished AnimalShelterController.cs

--- AnimalShelter/Controllers/AnimalShelterController.cs
+++ AnimalShelter/Controllers/AnimalShelterController.cs
@@ -15,6 +15,7 @@ namespace AnimalShelter.Controllers
   {
     _db = db;
   }
+
   [HttpGet]
   public async Task<ActionResult<IEnumerable<AnimalShelter>>> Get (int animalId, string animalName, string animalDescription, string animalType, string maleFemale, int animalAge)
   {
@@ -43,6 +44,7 @@ namespace AnimalShelter.Controllers
     {
       query = query.Where(e => e.AnimalAge == animalAge);
     }
+
     [HttpGet("{id}")]
     public async Task<ActionResult<Animal>> GetAnimal(int id)
     {
@@ -52,6 +54,58 @@ namespace AnimalShelter.Controllers
         return NotFound();
       }
       return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put (int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+      _db.Animals.Update(animal);
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAnimal (int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
     }
   }
 }
