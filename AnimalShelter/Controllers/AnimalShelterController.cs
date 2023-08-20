ae7adfc75f67f323c70b2455aa7f9d2a50dd8e79
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-08-20 12:12:38 -0700

    Finished 2 HttpGets, AnimalShelterController.cs still WIP.

--- AnimalShelter/Controllers/AnimalShelterController.cs
+++ AnimalShelter/Controllers/AnimalShelterController.cs
@@ -4,3 +4,54 @@ using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
 
 namespace AnimalShelter.Controllers
+{
+  [ApiController]
+  [Route("api/animalshelter/[controller]")]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public AnimalsController(AnimalShelterContext db)
+  }
+  {
+    _db = db;
+  }
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<AnimalShelter>>> Get (int animalId, string animalName, string animalDescription, string animalType, string maleFemale, int animalAge)
+  {
+    IQueryable<Animal> query = _db.Animals.AsQueryable();
+    if (animalId !- null)
+    {
+      query = query.Where(e => e.AnimalId == animalId);
+    }
+    if (animalName != null)
+    {
+      query = query.Where(e => e.AnimalName == name);
+    }
+    if (animalDescription != null)
+    {
+      query = query.Where(e => e.AnimalDescription == animalDescription);
+    }
+    if (animalType != null)
+    {
+      query = query.Where(e => e.AnimalType == animalType);
+    }
+    if (animalType != null)
+    {
+      query = query.Where(e => e.MaleFemale == maleFemale);
+    }
+    if (animalAge != null)
+    {
+      query = query.Where(e => e.AnimalAge == animalAge);
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      return animal;
+    }
+  }
+}
